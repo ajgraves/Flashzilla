633742da130baafc1faf529db1cb517383b3f7e5
Aaron Graves <ajgraves@ajgraves.com>
2024-09-15 17:05:34 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Aaron Graves on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
